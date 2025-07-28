b1c58f9ad4c67ed36e684b76be17260665881a85
Afan Alfi Andi <alfiandiafan@gmail.com>
2025-07-28 21:18:55 +0700

    update: sidebar and navbar

--- src/app/shared/layout/main/main.component.ts
+++ src/app/shared/layout/main/main.component.ts
@@ -4,10 +4,12 @@ import { LoadingComponent } from "../../components/loading/loading.component";
 import { LoadingService } from '../../components/loading/loading.service';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { BreadcrumbsComponent } from "../../components/breadcrumbs/breadcrumbs.component";
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet, LoadingComponent, CommonModule, SidebarComponent],
+  imports: [RouterOutlet, LoadingComponent, CommonModule, SidebarComponent, NavbarComponent, BreadcrumbsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
