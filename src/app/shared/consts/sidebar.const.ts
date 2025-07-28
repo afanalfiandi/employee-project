4172c2960331645cfd31c94585aa38e50bcf20b7
Afan Alfi Andi <alfiandiafan@gmail.com>
2025-07-29 00:03:42 +0700

    add: employee sidebar item

--- src/app/shared/consts/sidebar.const.ts
+++ src/app/shared/consts/sidebar.const.ts
@@ -3,5 +3,10 @@ export const SIDENAV = [
         label: "Dashboard",
         path: '/dashboard',
         icon: 'heroHome'
-    }
+    },
+    {
+        label: "Employee",
+        path: '/employee',
+        icon: 'heroUser'
+    },
 ]
