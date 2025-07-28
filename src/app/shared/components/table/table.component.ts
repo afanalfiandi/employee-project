5dc3696bdf9beefdc4a83c5933045fa93244afbc
Afan Alfi Andi <alfiandiafan@gmail.com>
2025-07-29 00:02:34 +0700

    add: reusable table component

---
+++ src/app/shared/components/table/table.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { TitleFormatterPipe } from '../../pipes/title-formatter.pipe';
+import { CommonModule } from '@angular/common';
+import { PaginationComponent } from "../pagination/pagination.component";
+
+@Component({
+  selector: 'app-table',
+  imports: [TitleFormatterPipe, CommonModule, PaginationComponent],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css'
+})
+export class TableComponent {
+  @Input() data!: any[];
+  @Input() columns!: string[];
+
+  getRowClass(col: string, row: any): string {
+    if (col === 'status') {
+      const statusClassMap: Record<string, string> = {
+        active: 'bg-blue-100/50 border-blue-900',
+        inactive: 'bg-red-100/50 border-red-800',
+      };
+
+      const baseClass = 'p-2 border-2';
+      const statusClass = statusClassMap[row[col]];
+
+      return statusClass ? `${baseClass} ${statusClass}` : '';
+    }
+
+    return '';
+  }
+}
