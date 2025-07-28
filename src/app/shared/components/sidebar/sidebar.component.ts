a64d9345931c90db8d4b60d61d85ad6825611608
Afan Alfi Andi <alfiandiafan@gmail.com>
2025-07-29 00:02:02 +0700

    add: adjust icons provider

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { SIDENAV } from '../../consts/sidebar.const';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { heroBuildingOffice, heroHome, heroMegaphone } from '@ng-icons/heroicons/outline';
+import { heroBuildingOffice, heroHome, heroUser } from '@ng-icons/heroicons/outline';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SidebarService } from './sidebar.service';
 import { CommonModule } from '@angular/common';
+import { APP_ICONS } from '../../consts/icons.const';
 
 @Component({
   selector: 'app-sidebar',
   imports: [NgIcon, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
-  providers: [provideIcons({ heroHome, heroBuildingOffice })]
+  providers: [provideIcons(APP_ICONS)]
 })
 export class SidebarComponent {
   private _sidebarService = inject(SidebarService);
---
+++ src/app/shared/consts/icons.const.ts
@@ -0,0 +1,10 @@
+import { heroChevronLeft, heroChevronRight, heroHome, heroUser, heroPlus, heroBuildingOffice } from '@ng-icons/heroicons/outline';
+
+export const APP_ICONS = {
+    heroChevronLeft,
+    heroChevronRight,
+    heroUser,
+    heroPlus,
+    heroBuildingOffice,
+    heroHome
+};
